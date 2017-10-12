322783bdfa16f1453dda31ccc35d9b103e01a017
diannao <diannao@xiaodiannao>
2017-10-12 18:04:51 +0300

    ne

--- src/com/evgen/coupons/logic/CustomerController.java
+++ src/com/evgen/coupons/logic/CustomerController.java
@@ -1,5 +1,19 @@
 package com.evgen.coupons.logic;
 
+import com.evgen.coupons.beans.Customer;
+import com.evgen.coupons.dao.CustomerDao;
+import com.evgen.coupons.exceptions.ApplicationException;
+
 public class CustomerController {
 
+	private CustomerDao customerDao;
+	
+	private CustomerController(CustomerDao customerDao) {
+		this.customerDao = customerDao;
+	}
+	public void customerCreate(Customer customer) throws ApplicationException {
+		this.customerDao.customerCreate(customer);
+	}
+	String activationToken = generateActivationToken();
+	customerDao.
 }
