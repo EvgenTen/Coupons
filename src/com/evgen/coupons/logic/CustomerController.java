cdff5e7c8588a88dd65aeaaf0ef544bcead16be1
irka <irka@irka-Inspiron-1520>
2017-10-16 14:11:45 +0300

    1

--- src/com/evgen/coupons/logic/CustomerController.java
+++ src/com/evgen/coupons/logic/CustomerController.java
@@ -11,37 +11,38 @@ import com.evgen.coupons.enums.ErrorType;
 import com.evgen.coupons.exceptions.ApplicationException;
 
 public class CustomerController implements IClientController {
-    CustomerDao customerDao = new CustomerDao();
+	CustomerDao customerDao = new CustomerDao();
 	CouponsDao couponsDao = new CouponsDao();
 	CompanyDao companyDao = new CompanyDao();
 	JoinedCustomerCoupon joinTable = new JoinedCustomerCoupon();
-	
+
 	private Customer customer;
-	
+
 	public CustomerController(Customer customer) {
 		this.customer = customer;
 	}
-	
-	 public boolean login(String custName, String password, ClientType clientType) throws ApplicationException {
-		
-		 if (clientType.equals(clientType.CUSTOMER)) {
-			 
-			 boolean isCustomerExist = customerDao.login(custName, password);	
-			 if (isCustomerExist) {
-					return true;
-			 }
-		 }
-		return false;
-	 }
-	 
-	 public void purchaseCoupon(Coupon coupon) throws ApplicationException {
-		 if(couponsDao.couponGetByCustomer(customer.setId(customer.getId())))
-			try {
-				joinTable.JoinCouponCreate(coupon.getId(), customer.getId());
-				//joinedTableDBDAO.customerPurchaseCoupon(this.customer, coupon);
-			} catch (Exception e) {
-				throw new ApplicationException(e, ErrorType.CAN_NOT_PURCHASE_THIS_COUPON);
+
+	public boolean login(String custName, String password, ClientType clientType) throws ApplicationException {
+
+		if (clientType.equals(clientType.CUSTOMER)) {
+
+			boolean isCustomerExist = customerDao.login(custName, password);
+			if (isCustomerExist) {
+				return true;
 			}
 		}
-	 
+		return false;
+	}
+
+	public boolean purchaseCoupon(Coupon coupon) throws ApplicationException {
+
+		if (couponsDao.couponGetById(coupon.getId()) != null
+				&& couponsDao.couponGetByCustomer(customer.getId()) == null) {
+
+			joinTable.JoinCouponCreate(coupon.getId(), customer.getId());
+			return true;
+
+		}
+		return false;
+	}
 }
